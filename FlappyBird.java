fa9f390953c46dfe0f85fc4ec96d40eeb658a5cf
Yohan Park <pyh95510@gmail.com>
2022-08-27 21:06:33 -0400

    fixing adding columns errors, now it adds both upper sides and down sides

--- FlappyBird.java
+++ FlappyBird.java
@@ -15,13 +15,14 @@ public class FlappyBird implements ActionListener {
 
 	public static FlappyBird flappyBird;
 	public final int WIDTH = 800, HEIGHT = 800;
-
+	
+	public int ticks, yMotion;
 	public Renderer renderer;
 	public Rectangle bird;
 	public ArrayList<Rectangle> columns;
 	public Random rand;
 	
-
+	
 	public FlappyBird() { // constructor
 		JFrame jframe = new JFrame();
 		Timer timer = new Timer(20, this);
@@ -41,17 +42,31 @@ public class FlappyBird implements ActionListener {
 		bird = new Rectangle(WIDTH / 2 - 10, HEIGHT / 2 - 10, 20, 20);
 		columns = new ArrayList<Rectangle>();
 		
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
+		
+		
 		
 		timer.start();
 
 	}
 	
-	public void addColumn() {
+	public void addColumn(boolean start) {
 		int space = 300;
 		int width = 100;
 		int height = 50 +rand.nextInt(300); //minimum height 50~ maximum height 300
+		if(start) {
+			columns.add(new Rectangle(WIDTH+width+columns.size()*300, HEIGHT - height - 120 , width , height)); //adding bottom part because we are subtracting -120 which is grass
+			columns.add(new Rectangle(WIDTH + width + (columns.size()-1)*300,0,width,HEIGHT-height-space));
+		}else {
+			columns.add(new Rectangle(columns.get(columns.size()-1).x+600, HEIGHT-height -120,width,height)); //adding bottom part because we are subtracting -120 which is grass
+			columns.add(new Rectangle(columns.get(columns.size()-1).x,0, width , HEIGHT - height - space));
+			
+		}
 		
-		columns.add(new Rectangle(WIDTH+width+columns.size()*300, HEIGHT-height -120,width,height));
+		
 		
 	}
 	
@@ -64,6 +79,23 @@ public class FlappyBird implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		ticks++;
+		int speed = 10;
+		
+		for(int i = 0 ; i<columns.size();i++) {
+			Rectangle column = columns.get(i);
+			column.x -= speed;
+			 
+		}
+		
+		if(ticks%2 == 0 && yMotion < 15) {
+			
+			yMotion += 2;
+		}
+		
+		bird.y +=yMotion;
+		
 		renderer.repaint();
 
 	}
@@ -82,6 +114,13 @@ public class FlappyBird implements ActionListener {
 
 		g.setColor(Color.red);
 		g.fillRect(bird.x, bird.y, bird.width, bird.height);
+		
+		for(Rectangle column: columns) {
+			paintColumn(g, column);
+		}
+		
+		
+		
 	}
 
 	public static void main(String[] args) {
