8fa77d5b1c2e6b8f1f3519e7a339977fec31c9ad
Yohan Park <pyh95510@gmail.com>
2022-08-27 22:37:03 -0400

    rendering done

--- FlappyBird.java
+++ FlappyBird.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
@@ -14,7 +16,7 @@ import java.util.Random;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-public class FlappyBird implements ActionListener,MouseListener {
+public class FlappyBird implements ActionListener,MouseListener,KeyListener {
 
 	public static FlappyBird flappyBird;
 	public final int WIDTH = 800, HEIGHT = 800;
@@ -40,6 +42,7 @@ public class FlappyBird implements ActionListener,MouseListener {
 		jframe.setTitle("Flappy Bird");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.addMouseListener(this);
+		jframe.addKeyListener(this);
 		jframe.setSize(WIDTH, HEIGHT);
 		jframe.setResizable(false);
 		jframe.setVisible(true);
@@ -158,7 +161,21 @@ public class FlappyBird implements ActionListener,MouseListener {
 					
 					gameOver = true;
 					
-					bird.x = column.x - bird.width;
+					if(bird.x<= column.x) {
+						
+						bird.x = column.x - bird.width;
+						
+					}
+					else {
+						if(bird.y != 0) {
+							bird.y = column.y - bird.height;
+						}
+						else if(bird.y < column.height) {
+							bird.y = column.height;
+						}
+					}
+					
+					
 				}
 			}
 			
@@ -252,6 +269,26 @@ public class FlappyBird implements ActionListener,MouseListener {
 	@Override
 	public void mouseExited(MouseEvent e) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			jump();
+		}
 		
 	}
 
